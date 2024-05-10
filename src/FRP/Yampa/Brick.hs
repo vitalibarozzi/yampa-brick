bc4cd2375437a39de3892a94581eb2b3e0cf38ce
João Vitor Vitali Barozzi <vitalibarozzi@pm.me>
2024-05-10 18:02:20 -0300

    polish and cleanup

--- src/FRP/Yampa/Brick.hs
+++ src/FRP/Yampa/Brick.hs
@@ -1,17 +1,13 @@
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE BlockArguments #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE ExplicitForAll #-}
-{-# LANGUAGE TypeApplications #-}
-module FRP.Yampa.Brick where
+module FRP.Yampa.Brick
+    (reactInitBrick)
+where
 
 
 import qualified FRP.Yampa as Yampa
 import qualified Brick
-import FRP.Yampa ((^>>), DTime, SF, Event(..),ReactHandle)
+import FRP.Yampa (SF, ReactHandle)
 import Control.Monad
 import Control.Concurrent
-import Control.Concurrent.Chan
 import Control.Monad.IO.Class
 import Brick.BChan as Brick
 import Graphics.Vty hiding (Event)
@@ -19,40 +15,49 @@ import qualified Graphics.Vty as VTY
 
 
 -----------------------------------------------------------
+-- | Starts the handle for the SF and exposes it together
+-- with a chan where the terminal events can be received.
 reactInitBrick ::
-    forall n s.
-    Ord n =>
     s ->
-    SF s (Brick.Widget n) ->
-    IO (Chan VTY.Event, ReactHandle s (Brick.Widget n))
+    SF s (Brick.Widget ()) ->
+    IO ( Chan VTY.Event
+       , ReactHandle s (Brick.Widget ())
+       )
+{-# INLINABLE reactInitBrick #-}
 reactInitBrick s sf = do
-    brickChan <- newBChan 100
+    brickChan <- newBChan 10
     eventChan <- newChan
-    () <- do
+    ()        <- initBrick brickChan eventChan
+    handle    <- Yampa.reactInit (pure s) (actuate brickChan) sf
+    return 
+        ( eventChan
+        , handle
+        )
+  where
+    initBrick brickChan eventChan = do
         makeVty <- mkVty <$> standardIOConfig
         vty     <- makeVty
-        void
+        void -- TODO could be done with immortal instead
             $ forkIO 
             $ void 
             $ Brick.customMain 
-                @n
                 vty 
                 makeVty 
                 (Just brickChan) 
-                (Brick.simpleApp Brick.emptyWidget) { 
-                    Brick.appDraw        = return,
-                    Brick.appHandleEvent = \case
-                        Brick.AppEvent wid -> Brick.put wid -- triggers Brick.appDraw
-                        Brick.VtyEvent e   -> liftIO (writeChan eventChan e)
-                        __________________ -> pure ()
+                (Brick.simpleApp Brick.emptyWidget) {
+                    Brick.appDraw          = return, -- we use the state for widgets
+                    Brick.appHandleEvent = \e -> 
+                        case e of
+                            Brick.AppEvent wid -> Brick.put wid -- triggers Brick.appDraw
+                            Brick.VtyEvent vte -> liftIO (writeChan eventChan vte)
+                            __________________ -> pure ()
                 }
                 Brick.emptyWidget
-    handle <- Yampa.reactInit (pure s) (actuate brickChan) sf
-    return (eventChan, handle)
-  where
-    actuate brickChan handle updated widget = do
+    actuate brickChan _ updated widget = do
         -- sends the widget to be rendered by the brick thread
-        when updated do Brick.writeBChan brickChan widget 
-        -- keeps the yampa handler alive afterwards
+        when updated $ do 
+            threadDelay 1
+            Brick.writeBChan brickChan widget 
+        -- keeps the yampa handler alive afterwards. no shutdown at the moment
         pure False
 
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,41 +1,20 @@
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE BlockArguments #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE Arrows #-}
 import FRP.Yampa.Brick
+import FRP.Yampa ((>>>),arr)
 import qualified FRP.Yampa as Yampa
-import FRP.Yampa (Event(..))
 import qualified Brick
 import qualified Data.Text as Text
-import Data.IORef
-import System.IO.Unsafe
-import System.IO
-import System.Exit
 import System.Timeout
 import Control.Concurrent
-import Control.Concurrent.Chan
 import Control.Monad
-import Graphics.Vty.Input.Events
 
 
 main :: IO ()
 main = do
-    (chan,handle) <- reactInitBrick @() () do
-        proc s -> do
-            t <- Yampa.time -< s
-            let f = Brick.txt 
-                        $  Text.pack 
-                        $ "State: " 
-                        <> show s 
-                        <> "\n Time  : " 
-                        <> show t
-            Yampa.returnA -< f
-    forever do
+    (chan,handle) <- reactInitBrick () $ Yampa.time >>> arr (Brick.txt . Text.pack . show)
+    forever $ do
         threadDelay    100000
         mev <- timeout 1000 $ readChan chan
         case mev of
             Nothing -> pure ()
-            Just ev -> undefined
+            Just ev -> error ("received input from Vty: "<>show ev)
         Yampa.react handle (0.1, Just ())
